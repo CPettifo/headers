d08f108e98d48aa274cccd36d166f311dbaa9c28
CPettifo <cpettifo@myune.edu.au>
2025-08-21 14:52:03 +0200

    prototype for passing between collector & processor

--- services/collector/app.py
+++ services/collector/app.py
@@ -1,1 +1,25 @@
-print("Your ip address is localhost")
+import requests
+import time
+
+processor_url = "http://processor:5000/process"
+
+def main():
+    headers = collect_headers()
+    try:
+        response = requests.post(processor_url, json=headers)
+        print("Sent", headers, "Response", response.status_code)
+    except Exception as e:
+        print("Error: ", e)
+
+
+def collect_headers():
+    # for now just return wikipedia's ip
+    return {
+        "ip": "208.80.154.224",
+        "user_agent": "Mozilla/5.0"
+    }
+
+
+
+if __name__ == "__main__":
+    main()
