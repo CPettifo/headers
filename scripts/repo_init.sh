63e412fe28802595b340be6d4e2920b65f11fb0f
CPettifo <cpettifo@myune.edu.au>
2025-08-21 13:51:31 +0200

    complete shell script for repo structure

--- scripts/repo_init.sh
+++ scripts/repo_init.sh
@@ -1,1 +1,40 @@
-# Initialising the repo directory structure using shell commands because why not
+#!/bin/bash
+
+# Initialising the repo directory structure using shell commands
+
+# starting with the root directory (parent of scripts dir)
+cd ..
+
+# create gitignore file with the .env file
+echo .env >> .gitignore
+
+touch LICENSE README.md .env .env.example docker-compose.yml 
+
+# create db project folder and navigate to it
+mkdir db && cd db
+
+touch init.sql sample.sql README.md
+
+# create services folder and frontend subfolder and navigate to it
+cd .. && mkdir -p services/frontend && cd services/frontend
+
+touch Dockerfile README.md
+
+# create collector service subfolder
+cd .. && mkdir collector && cd collector && mkdir tests
+
+touch Dockerfile README.md app.py requirements.txt
+
+# create processor service subfolder
+cd .. && mkdir processor && cd processor && mkdir tests
+
+touch Dockerfile README.md app.py requirements.txt
+
+# create analytics service subfolder
+cd .. && mkdir analytics && cd analytics && mkdir tests sql
+
+touch Dockerfile README.md app.py requirements.txt
+
+cd ../../scripts
+
+touch simulate_visits.py parse_ua.py
