3891a91b0c706fede14b7f737be9872be9cf551d
Craig Pettifor <craig@pettifor.tech>
2025-10-23 14:32:07 +0200

    turn on debug mode

--- services/processor/processor.py
+++ services/processor/processor.py
@@ -63,4 +63,4 @@ def process():
     return jsonify({"status": "ok", "data": response})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000 debug=True)
