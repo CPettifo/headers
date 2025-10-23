35b65a3fef5bbaf68db1e2b40bbfb97f04c7db93
Craig Pettifor <craig@pettifor.tech>
2025-10-23 10:41:14 +0200

    add todo

---
+++ services/analytics/app.py
@@ -0,0 +1,3 @@
+# TODO:
+    # This service will be used for the simplified analytics dashboard, displaying information to all
+    # visitors of the most popular browsers, countries, for the last 30 days
